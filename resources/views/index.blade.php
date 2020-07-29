f9789aa1ed5c277b53a71ac59500639d490baa24
Clever Cracker <Clavercracker.gul@gmail.com>
2020-07-29 16:25:48 +0500

    After First Lecture

---
+++ resources/views/index.blade.php
@@ -0,0 +1,21 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="container">
+        <div class="jumbotron">
+            <h1 class="display-3">Index</h1>
+            <p class="lead">Welcome to Cs-17</p>
+            <hr class="my-2">
+            <p>More info</p>
+            <p class="lead">
+                <a class="btn btn-primary btn-lg" href="Jumbo action link" role="button">Jumbo action name</a>
+            </p>
+        </div>
+    </div>
+
+
+@section('footer')
+    <script>
+        alert("Knock Knock");
+    </script>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,61 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+// Route::get('/about',function(){
+//     return "Knock Knock";
+// });
+
+// Route::get('/contact', function () {
+//     return "<h1>Contact Page</h1>";
+// });
+
+// !Simple Route
+
+// Route::get('post/{id}', function ($id) {
+//     return "THis is ID = $id";
+// });
+
+
+// Route::get('post/temp/example',array('as'=>"temp.name" , function () {
+//     return route("temp.name");
+// }));
+// Route::get('post/{id}/{name}/', function ($id,$name) {
+                
+//     return "THis is ID = $id And Name = $name";
+// });
+
+// ! Route Controller
+
+// Route::get('/post','PostsController@index');
+
+// ! View Using Route
+
+// Route::get('/', function () {
+//     return view('index');
+// });
+
+// Route::get('/about',function(){
+//     return "Knock Knock";
+// });
+
+// Route::get('/contact', function () {
+//     return view("contact");
+// });
+
+// ! Passing Data in View
+
+Route::get('/post/{id}', function ($id) {
+    return view('post')->with('id',$id);
+});
+
+Route::get('/post/{id}/{name}', function ($id,$name) {
+    return view('post' ,compact('id','name'));
+});
+
 Route::get('/', function () {
-    return view('welcome');
+    return view("index");
 });
